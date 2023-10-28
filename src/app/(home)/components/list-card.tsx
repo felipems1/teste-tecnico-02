e26d26f31f512e63e24536ed0216330dc730e086
felipe <083felype@gmail.com>
2023-10-28 11:15:27 -0300

    feat: add ListCard and SearchHero components

---
+++ src/app/(home)/components/list-card.tsx
@@ -0,0 +1,17 @@
+'use client'
+
+import { HeroesContext } from '@/providers/heroes'
+import { useContext } from 'react'
+import { Card } from './card'
+
+export function ListCard() {
+  const { heroes } = useContext(HeroesContext)
+
+  return (
+    <div className="grid grid-cols-4 gap-12 px-6">
+      {heroes.map((hero) => (
+        <Card key={hero.id} hero={hero} />
+      ))}
+    </div>
+  )
+}
---
+++ src/components/ui/search-hero.tsx
@@ -0,0 +1,27 @@
+'use client'
+
+import { HeroesContext } from '@/providers/heroes'
+import { Search } from 'lucide-react'
+import { useContext, ChangeEvent } from 'react'
+
+export function SearchHero() {
+  const { filter, setFilter } = useContext(HeroesContext)
+
+  function handleSearchHero(event: ChangeEvent<HTMLInputElement>) {
+    setFilter(event.target.value)
+  }
+
+  return (
+    <div className="fixed right-2 flex-1 py-10 px-3">
+      <div className="border-blue-500 border-2 rounded-xl bg-zinc-900 p-2 flex gap-2 w-full">
+        <Search />
+        <input
+          type="text"
+          value={filter}
+          onChange={handleSearchHero}
+          className="bg-zinc-900 outline-none max-w-[80%]"
+        />
+      </div>
+    </div>
+  )
+}
